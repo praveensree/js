01ffe3bac69ce852f56a65744f6bdef04e180a90
Praveen P <praveen.padmanabhan@bhavnacorp.com>
2022-02-14 11:42:10 +0530

    destructuring

--- destructure.js
+++ destructure.js
@@ -28,3 +28,29 @@ const [a, ...b] = [1, 2, 3];
 console.log(a); 
 console.log(b);
 
+function iteration(){
+    const users = [
+        { 
+            'name': 'Alex',
+            'address': '15th Park Avenue',
+            'age': 43
+        },
+        { 
+            'name': 'Bob',
+            'address': 'Canada',
+            'age': 53
+        },
+        { 
+            'name': 'Carl',
+            'address': 'Bangalore',
+            'age': 26
+        }
+    ];
+
+    for(let { name, age } of users) {
+        console.log(`${name} is ${age} years old!`);
+    }
+}
+
+iteration();
+
